06ae0ccdeccd099a42c82b47761a6498dd5bab35
tedem <tedemdev@gmail.com>
2021-02-17 14:00:31 +0300

    build: add tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import "../styles/app.css";
+
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: [...defaultTheme.fontFamily.sans],
+      mono: [...defaultTheme.fontFamily.mono],
+      system: defaultTheme.fontFamily.sans,
+    },
+    extend: {
+      colors: {
+        emerald: colors.emerald,
+        fuchsia: colors.fuchsia,
+        violet: colors.violet,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
