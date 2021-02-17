ab26d96b0f258c96bef79d11f1db4a735a46c58a
tedem <tedemdev@gmail.com>
2021-02-17 18:27:27 +0300

    feat: add layout footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -3,7 +3,11 @@ import Holder from "../layout/holder";
 export default function Footer() {
   return (
     <footer>
-      <Holder>Footer</Holder>
+      <Holder>
+        <div className="flex items-center py-8 border-t border-gray-700">
+          <div>&copy; 2020 tedem.dev</div>
+        </div>
+      </Holder>
     </footer>
   );
 }
