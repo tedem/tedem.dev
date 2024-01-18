280771247c0fe3bcecc89b304ba3576129dac5b7
Medet Erdal <hello@tedem.dev>
2024-01-18 22:39:14 +0300

    feat(views): add latest articles section
    Added latest articles section to home page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <x-layouts.app>
     <div>
         @include('home.self-introduction')
+        @include('home.latest-articles')
     </div>
 </x-layouts.app>
---
+++ resources/views/home/latest-articles.blade.php
@@ -0,0 +1,190 @@
+<section class="py-12" id="latest-articles">
+    <div class="mx-auto max-w-7xl max-lg:px-4 lg:px-8">
+        <div class="grid grid-cols-1 gap-16 lg:grid-cols-3">
+            <div>
+                <div class="space-y-6 lg:sticky lg:top-8">
+                    <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+                        Latest articles
+                    </h2>
+                    <p class="text-balance text-slate-600">
+                        I like to learn new things and share what I have learnt.
+                        You can find the main software related articles here.
+                    </p>
+                    <div
+                        class="inline-block rounded-full border border-dashed border-slate-300 px-3 py-px text-xs/6 font-medium text-slate-900">
+                        3 articles in total
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-2 space-y-12">
+                <article>
+                    <div class="mb-6">
+                        <div class="inline-flex items-center gap-x-4">
+                            <span class="inline-flex h-4 w-0.5 rounded-full bg-slate-300" aria-hidden="true"></span>
+                            <time class="text-sm font-medium text-slate-600">16 Jun, 2024</time>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 class="text-2xl font-semibold text-slate-950">
+                            <a href="#">
+                                The Future of Web Development: Our Predictions for 2023
+                            </a>
+                        </h2>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-balance line-clamp-3 text-base">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, deserunt eligendi! Voluptate
+                            nihil optio error. Ipsa rem quas tempore, consequuntur, repudiandae, hic ex laboriosam
+                            praesentium delectus magnam inventore nihil amet!
+                        </p>
+                    </div>
+                    <div class="mt-6">
+                        <ul class="flex gap-2">
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-slate-700" aria-hidden="true"></span>
+                                <span>Sales</span>
+                            </li>
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-blue-700" aria-hidden="true"></span>
+                                <span>Marketing</span>
+                            </li>
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-green-700" aria-hidden="true"></span>
+                                <span>SEO</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mt-12" aria-hidden="true">
+                        <div class="flex items-center gap-4">
+                            <div class="h-px w-12 rounded-full bg-slate-950"></div>
+                            <div class="h-px flex-1 rounded-full bg-slate-200"></div>
+                            <div>
+                                <a class="text-sm/none font-medium lowercase text-slate-900 transition hover:text-slate-700"
+                                    href="#">
+                                    Read more
+                                </a>
+                            </div>
+                            <div class="h-px w-12 rounded-full bg-slate-200"></div>
+                        </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div class="mb-6">
+                        <div class="inline-flex items-center gap-x-4">
+                            <span class="inline-flex h-4 w-0.5 rounded-full bg-slate-300" aria-hidden="true"></span>
+                            <time class="text-sm font-medium text-slate-600">16 Jun, 2024</time>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 class="text-2xl font-semibold text-slate-950">
+                            <a href="#">
+                                The Future of Web Development: Our Predictions for 2023
+                            </a>
+                        </h2>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-balance line-clamp-3 text-base">
+                            Donate: If you use this site regularly and would like to help keep the site on the Internet,
+                            please consider donating a small sum to help pay for the hosting and bandwidth bill. There
+                            is no minimum donation, any sum is appreciated - click here to donate using PayPal. Thank
+                            you for your support.
+                        </p>
+                    </div>
+                    <div class="mt-6">
+                        <ul class="flex gap-2">
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-slate-700" aria-hidden="true"></span>
+                                <span>Sales</span>
+                            </li>
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-blue-700" aria-hidden="true"></span>
+                                <span>Marketing</span>
+                            </li>
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-green-700" aria-hidden="true"></span>
+                                <span>SEO</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mt-12" aria-hidden="true">
+                        <div class="flex items-center gap-4">
+                            <div class="h-px w-12 rounded-full bg-slate-950"></div>
+                            <div class="h-px flex-1 rounded-full bg-slate-200"></div>
+                            <div>
+                                <a class="text-sm/none font-medium lowercase text-slate-900 transition hover:text-slate-700"
+                                    href="#">
+                                    Read more
+                                </a>
+                            </div>
+                            <div class="h-px w-12 rounded-full bg-slate-200"></div>
+                        </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div class="mb-6">
+                        <div class="inline-flex items-center gap-x-4">
+                            <span class="inline-flex h-4 w-0.5 rounded-full bg-slate-300" aria-hidden="true"></span>
+                            <time class="text-sm font-medium text-slate-600">16 Jun, 2024</time>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 class="text-2xl font-semibold text-slate-950">
+                            <a href="#">
+                                The Future of Web Development: Our Predictions for 2023
+                            </a>
+                        </h2>
+                    </div>
+                    <div class="mt-6">
+                        <p class="text-balance line-clamp-3 text-base">
+                            Donate: If you use this site regularly and would like to help keep the site on the Internet,
+                            please consider donating a small sum to help pay for the hosting and bandwidth bill. There
+                            is no minimum donation, any sum is appreciated - click here to donate using PayPal. Thank
+                            you for your support.
+                        </p>
+                    </div>
+                    <div class="mt-6">
+                        <ul class="flex gap-2">
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-slate-700" aria-hidden="true"></span>
+                                <span>Sales</span>
+                            </li>
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-blue-700" aria-hidden="true"></span>
+                                <span>Marketing</span>
+                            </li>
+                            <li
+                                class="flex items-center gap-2 rounded-full border border-slate-200 bg-white px-3 py-1 text-xs/4 font-medium">
+                                <span class="size-1.5 inline-block rounded-full bg-green-700" aria-hidden="true"></span>
+                                <span>SEO</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mt-12" aria-hidden="true">
+                        <div class="flex items-center gap-4">
+                            <div class="h-px w-12 rounded-full bg-slate-950"></div>
+                            <div class="h-px flex-1 rounded-full bg-slate-200"></div>
+                            <div>
+                                <a class="text-sm/none font-medium lowercase text-slate-900 transition hover:text-slate-700"
+                                    href="#">
+                                    Read more
+                                </a>
+                            </div>
+                            <div class="h-px w-12 rounded-full bg-slate-200"></div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</section>
