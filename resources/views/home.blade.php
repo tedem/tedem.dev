5919dfb4b10d865920c89f684e6316ac87b94b91
Medet Erdal <hello@tedem.dev>
2024-12-05 15:48:40 +0300

    feat(views): add self introduction section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 <x-layouts.app>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            Home
-        </div>
+    <div>
+        @include('home.self-introduction')
     </div>
 </x-layouts.app>
