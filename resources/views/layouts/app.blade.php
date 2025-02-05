de0f77c58c41d7f19759cf4291e8d9b4ae922ef3
Medet Erdal <hello@tedem.dev>
2025-02-05 19:47:31 +0300

    feat: create home view and layout components

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html class="scroll-smooth" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'tedem') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="min-h-lvh bg-white font-sans text-gray-950 antialiased selection:bg-teal-100 selection:text-teal-700">
+    <main>
+        {{ $slot }}
+    </main>
+</body>
+
+</html>
