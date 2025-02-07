57976f8a3b98dde1e286c9573778e9225319caee
Medet Erdal <hello@tedem.dev>
2025-02-07 23:21:11 +0300

    refactor(views): update text color for improved visibility in labels

--- resources/views/partials/projects.blade.php
+++ resources/views/partials/projects.blade.php
@@ -2,7 +2,7 @@
     {{-- Label --}}
     <div class="absolute -right-px top-8 translate-x-full max-xl:hidden" aria-hidden="true">
         <span
-            class="select-none uppercase leading-loose tracking-widest text-gray-400 [writing-mode:vertical-rl] dark:text-gray-500">
+            class="select-none uppercase leading-loose tracking-widest text-gray-500 [writing-mode:vertical-rl] dark:text-gray-400">
             // Projects
         </span>
     </div>
