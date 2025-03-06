2ea630f73049db9346e2f996cd874a1391ee22ef
Medet Erdal <hello@tedem.dev>
2025-03-06 20:38:37 +0300

    fix(js): enhance autofocus and wire:navigate interactions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 import './bootstrap';
+
+// HTML: autofocus
+// Fix for autofocus not focusing on the element when navigating to a new page.
+document.addEventListener('livewire:navigated', () => {
+    const elements = document.querySelectorAll('[autofocus]');
+
+    elements.forEach(element => {
+        if (!element.disabled && element.offsetParent !== null) {
+            element.focus();
+            return false;
+        }
+    });
+});
+
+// HTML: wire:navigate
+// Fix for wire:navigate not focusing on the element when clicked.
+document.addEventListener('livewire:navigated', () => {
+    const elements = document.querySelectorAll('[wire\\:navigate]');
+
+    elements.forEach(element => {
+        element.addEventListener('mousedown', () => {
+            if (!element.classList.contains('focus:outline-none')) {
+                element.classList.add('focus:outline-none');
+            }
+
+            element.focus();
+        });
+    });
+});
