3d908423fa1f87f35e165c05408d645828a345dc
Medet Erdal <hello@tedem.dev>
2025-03-08 14:45:02 +0300

    refactor(js): move appearance mode controller to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,18 @@
 import './bootstrap';
 
+// Appearance Mode Controller
+// This controller is used to manage the appearance mode of the application.
+// The appearance mode can be set to 'light', 'dark', or 'system'.
+Alpine.data('appearanceModeController', () => ({
+    appearanceModeStatus: localStorage.getItem('appearance.mode') || 'system',
+
+    applyAppearanceMode(mode) {
+        this.appearanceModeStatus = mode;
+
+        window.appearanceManager.applyAppearanceMode(mode);
+    },
+}));
+
 // HTML: autofocus
 // Fix for autofocus not focusing on the element when navigating to a new page.
 document.addEventListener('livewire:navigated', () => {
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,13 +2,3 @@ import axios from 'axios';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-Alpine.data('appearanceModeController', () => ({
-    appearanceModeStatus: localStorage.getItem('appearance.mode') || 'system',
-
-    applyAppearanceMode(mode) {
-        this.appearanceModeStatus = mode;
-
-        window.appearanceManager.applyAppearanceMode(mode);
-    },
-}));
