a7c0b730fa9f2c9db6d6300f2bbd71ce0c0adf26
Medet Erdal <hello@tedem.dev>
2025-03-09 23:29:51 +0300

    feat(views): add dropdown keybind component

---
+++ resources/views/components/dropdown/keybind.blade.php
@@ -0,0 +1,5 @@
+<div class="ml-auto inline-flex rounded bg-gray-950/5 px-1 py-0.5 dark:bg-white/5">
+    <span class="text-xs font-medium text-gray-500 dark:text-gray-300">
+        {{ $slot }}
+    </span>
+</div>
