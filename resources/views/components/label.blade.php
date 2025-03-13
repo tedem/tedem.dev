23ad2482e3afadac845b90d4675b0be7b1dc9b6b
Medet Erdal <hello@tedem.dev>
2025-03-13 18:03:41 +0300

    refactor(views): update label component

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,5 +1,6 @@
 @props(['value'])
 
-<label {{ $attributes->class(['inline-flex text-sm font-medium select-none text-gray-950 dark:text-white px-4']) }}>
+<label
+    {{ $attributes->merge(['class' => 'inline-flex text-sm font-medium select-none text-gray-950 dark:text-white px-4']) }}>
     <span>{{ $value }}</span>
 </label>
