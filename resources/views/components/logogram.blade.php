8ae4225a90a6472f94c0ed47838e0d76c02a6bce
Medet Erdal <hello@tedem.dev>
2025-03-13 18:03:24 +0300

    refactor(views): update logogram component

--- resources/views/components/logogram.blade.php
+++ resources/views/components/logogram.blade.php
@@ -1,27 +1,29 @@
 @props(['size' => 'md', 'as' => '!'])
 
 @php
-    $sizeProperties = [
+    $sizeClasses = match ($size) {
         'xs' => '[--size:calc(var(--spacing)*6)]',
         'sm' => '[--size:calc(var(--spacing)*8)]',
         'md' => '[--size:calc(var(--spacing)*10)]',
         'lg' => '[--size:calc(var(--spacing)*12)]',
         'xl' => '[--size:calc(var(--spacing)*14)]',
-    ][$size];
+        default => '[--size:calc(var(--spacing)*10)]',
+    };
 
-    $textSizeClass = [
+    $textSizeClasses = match ($size) {
         'xs' => 'text-xs',
         'sm' => 'text-sm',
         'md' => 'text-base',
         'lg' => 'text-lg',
         'xl' => 'text-xl',
-    ][$size];
+        default => 'text-base',
+    };
 
-    $classes = implode(' ', [
+    $classes = collect([
         'flex size-(--size) select-none items-center justify-center rounded-full bg-gray-950 text-gray-50 dark:bg-gray-50 dark:text-gray-950',
-        $textSizeClass,
-        $sizeProperties,
-    ]);
+        $textSizeClasses,
+        $sizeClasses,
+    ])->filter()->implode(' ');
 @endphp
 
 @if ($as === 'link')
