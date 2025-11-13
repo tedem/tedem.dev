7c1e4e7ef5d25339ce4204e1dcd6197862352cf2
Medet Erdal <hello@tedem.dev>
2025-11-13 22:43:46 +0300

    chore: update coding style configuration in Rector

--- rector.php
+++ rector.php
@@ -18,6 +18,6 @@ return RectorConfig::configure()
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
+        codingStyle: true,
     )
     ->withPhpSets();
